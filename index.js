a32bae205a61b6f211ca612db27a65c90aab4462
Sahil Ingale <60597512+sahilingale629@users.noreply.github.com>
2023-11-12 15:50:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+
+var name = "Dice" + randomNumber1 + ".png";
+var sor = "./images/" + name;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", sor);
+
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+var name = "Dice" + randomNumber2 + ".png";
+var sor = "./images/" + name;
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", sor);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player One Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player Two Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
+alert("working");
