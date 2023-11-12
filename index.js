f52749ddd92edab259ac7f28be1dd1033313c037
Sahil Ingale <60597512+sahilingale629@users.noreply.github.com>
2023-11-12 16:01:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1 = Math.floor(Math.random() * 6 + 1);
 
 var name = "dice" + randomNumber1 + ".png";
-var sor = "./images/" + name;
+var sor = "images/" + name;
 
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", sor);
@@ -9,7 +9,7 @@ image1.setAttribute("src", sor);
 var randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
 var name = "dice" + randomNumber2 + ".png";
-var sor = "./images/" + name;
+var sor = "images/" + name;
 
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", sor);
